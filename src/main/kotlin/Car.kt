f0c730b9c8edc86c72a0de4dc0330eb304d8637f
Eden Tewelde <eden.tewelde@th-bingen.de>
2017-08-06 22:57:25 +0200

    edit class Car

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,22 +1,10 @@
-class Car(val wantsToDrive: Boolean) {
+class Car(val wantsToDrive: Boolean, val roadNetwork : Network) {
 
-    val listOfCars : MutableList<Car> = mutableListOf()
-    val listOfCarsDriving : MutableList<Car> = mutableListOf()
-
-    // when a new car is initialized it is added to the listOfCars
+    // when a new car is initialized it is added to the listOfCars of the Network
     init {
-        listOfCars.add(this)
+        roadNetwork.listOfCars.add(this)
         if (wantsToDrive == true) {
-            listOfCarsDriving.add(this)
-        }
-    }
-
-    // I guess this is not necessary
-    fun announce() : Boolean {
-        if (wantsToDrive == true) {
-            return true
-        } else {
-            return false
+            roadNetwork.listOfCarsDriving.add(this)
         }
     }
 }
