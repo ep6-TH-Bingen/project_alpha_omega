17dc4873fb5f71711529b0f7a93da091f658123e
Ralf Breitenstein <ralf.breitenstein@gmx.de>
2017-08-07 22:19:12 +0200

    starting with csv import

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+
+import com.univocity.parsers.common.record.Record
+import com.univocity.parsers.csv.CsvParser
+import com.univocity.parsers.csv.CsvParserSettings
+import com.univocity.parsers.csv.CsvWriter
+import com.univocity.parsers.csv.CsvWriterSettings
+
+fun main(args: Array<String>) {
+
+
+    private fun readCarFromCsv(fileName: String): Car {
+        val settings = CsvParserSettings()
+        settings.format.setLineSeparator("\n")
+        settings.isHeaderExtractionEnabled = true
+
+        val csvParser = CsvParser(settings)
+        val reader = FileAccess().getReader(fileName)
+        val customerRows: MutableList<Record> = csvParser.parseAllRecords(reader)
+
+        for (record in customerRows) {
+            val carID = record.values.get(0)
+            val carStatus = record.values.get(1)
+
+        }
+
+
+
+
+
+
+}
