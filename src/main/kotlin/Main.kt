da076a1de64444fbb9e6e5db7e3a47b8590ab0a7
Ralf Breitenstein <ralf.breitenstein@gmx.de>
2017-08-08 16:21:51 +0200

    reformatted code, started with 'fun main', need help for creating cars

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,28 +7,51 @@ import com.univocity.parsers.csv.CsvWriterSettings
 
 fun main(args: Array<String>) {
 
+    val cars = readNetworkFromCsv("/cars.csv")
 
+    println(cars)
+/*
+    val car1 = cars.listOfCars.get(0)
+    val car2 = cars.listOfCars.get(1)
+    val car3 = cars.listOfCars.get(2)
+    val car4 = cars.listOfCars.get(3)
+    val car5 = cars.listOfCars.get(4)
+    val car6 = cars.listOfCars.get(5)
+    val car7 = cars.listOfCars.get(6)
+    val car8 = cars.listOfCars.get(7)
+    val car9 = cars.listOfCars.get(8)
+    val car10 = cars.listOfCars.get(9)
+    val car11 = cars.listOfCars.get(10)
+    val car12 = cars.listOfCars.get(11)
+    val car13 = cars.listOfCars.get(12)
+    val car14 = cars.listOfCars.get(13)
+    val car15 = cars.listOfCars.get(14)
+*/
 
         }
 
 
-    private fun readCarFromCsv(fileName: String): Network {
+private fun readNetworkFromCsv(fileName: String): Network {
     //Changed the return "Car" into "Network", because there is the "listOfCars" ...?? is that the right way?
 
-        val settings = CsvParserSettings()
-        settings.format.setLineSeparator("\n")
-        settings.isHeaderExtractionEnabled = true
+    val settings = CsvParserSettings()
+    settings.format.setLineSeparator("\n")
+    settings.isHeaderExtractionEnabled = true
 
-        val csvParser = CsvParser(settings)
-        val reader = FileAccess().getReader(fileName)
-        val carRows: MutableList<Record> = csvParser.parseAllRecords(reader)
+    val csvParser = CsvParser(settings)
+    val reader = FileAccess().getReader(fileName)
+    val carRows: MutableList<Record> = csvParser.parseAllRecords(reader)
 
     // same here with "Network()"
-        val car = Network()
+    // The closing bracket after "Network" is still underlined...
+    val car = Network()
 
-        for (record in carRows) {
-            val carID = record.values.get(0)
-            val carStatus = record.values.get(1)
+    for (record in carRows) {
+        val carID = record.values.get(0)
+        val carStatus = record.values.get(1)
 
 
-        }
+    }
+
+    return car
+}
