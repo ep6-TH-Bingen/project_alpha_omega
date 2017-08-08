8c95eb9024b05420dcf397fbb60b477be7c97493
Ralf Breitenstein <ralf.breitenstein@gmx.de>
2017-08-08 11:23:01 +0200

    commit of Network.kt

--- src/main/kotlin/Network.kt
+++ src/main/kotlin/Network.kt
@@ -1,5 +1,25 @@
-/**
- * Created by Ralf on 08.08.2017.
- */
-class Network {
+
+class Network(val capacity : Int) {
+
+    val listOfCars : MutableList<Car> = mutableListOf()
+    val listOfCarsDriving : MutableList<Car> = mutableListOf()
+
+    fun overCapacity() : Boolean {
+        // tallies up the number of cars on the road
+        val numberOfCarsDriving = listOfCarsDriving.size
+        // compares it to its own capacity
+        if (this.capacity >= numberOfCarsDriving) {
+            return false
+        } else {
+            return true
+        }
+    }
+
+    fun analyzeNetwork() {
+        val overCapacity = this.overCapacity()
+        for (car in this.listOfCarsDriving) {
+            car.delayed = overCapacity
+        }
+    }
+
 }
